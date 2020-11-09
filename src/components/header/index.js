2b1fc9d48c1d7291b56bb6aaaa9c4be891a355b8
Eddie Sevilla <eddiesevilla@Eddies-Air.attlocal.net>
2020-11-08 21:35:14 -0600

    updates

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+
+import React from "react"
+import "./style.css"
+
+
+function header() {
+    return (
+        <div>
+            <h1>Employee Directory</h1>
+        </div>
+
+    )
+}
+
+export default header
---
+++ src/components/tableData/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Moment from "react-moment";
+import "./style.css"
+
+function TableData(props) {
+    return (
+        <table className="employeeTable">
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th onClick={props.sortByName}>Name</th>
+                    <th>Phone</th>
+                    <th>E-mail</th>
+                </tr>
+            </thead>
+
+            <tbody className="">
+                {props.results.map(result =>
+                    <tr className="table" key={result.login.uuid}>
+                        <td><img src={result.picture.thumbnail} alt={"profile image for " + result.name.first + " " + result.name.last} className="" /></td>
+                        <td>{result.name.first + " " + result.name.last}</td>
+                        <td>{result.phone}</td>
+                        <td className="employeeEmail"><a href={result.emil}>{result.email}</a></td>
+
+                    </tr>
+                )}
+            </tbody>
+        </table>
+
+    )
+}
+
+export default TableData
